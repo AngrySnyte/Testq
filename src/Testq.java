9d71be55053cfc41eb11ecf50681fc50cedf19cc
akulebyakin <spyroo@MAIL.RU>
2018-03-06 13:25:01 +0300

    fix3

--- src/Testq.java
+++ src/Testq.java
@@ -1,5 +1,8 @@
 public class Testq {
     public static void main(String[] args) {
-        System.out.print("Helloworld");
+        for (int i = 100; i < 999; i++) {
+            int ad = i % 9;
+            System.out.format("%d%d%n", i, ad);
+        }
     }
 }
